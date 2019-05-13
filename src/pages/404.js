657fbd1ff30fc62dd8228b3601af724bf16fe29a
Messan christian <messanchristian@hotmail.com>
2019-05-13 09:02:56 +0000

    fix reloadin

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,7 +3,7 @@ import React from "react"
 
 export default function NotFound() {
   if (typeof window !== 'undefined') {
-    window.location = '/pourquoi-apprendre-a-coder';
+    window.location = '/5-raisons-pour-apprendre-a-coder';
   }
 
   return null;
