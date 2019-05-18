94a8633df262a9469bb5be8572010ff824bd4d1e
Messan christian <messanchristian@hotmail.com>
2019-05-18 18:44:04 +0000

    New version 1.0.3

--- src/components/HeroArticles.js
+++ src/components/HeroArticles.js
@@ -1,45 +1,59 @@
-import React from "react"
+import React ,{Component} from "react"
 import TypesIcons from "./TypesIcons"
 import "./css/HeroArticles.css"
 import styled from "styled-components"
 const slugify = require("@sindresorhus/slugify")
-const HandleImage = name => {
-  return require(`../images/wordpress/${name}.jpg`)
-}
 
-const HandlePath = location =>{
-  var path ="";
-  for (let i=1;i<location.length;i++){
-    path+=location[i];
+
+class Hero extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      location:window.location.pathname
+     };
   }
-  return path
+  render() {
+    const HandleImage = name => {
+      return require(`../images/wordpress/${name}.jpg`)
+    }
+    
+    const HandlePath = location =>{
+      var path ="";
+      for (let i=1;i<location.length;i++){
+        path+=location[i];
+      }
+      return path
+    }
+    
+    var myPath = window.location.pathname;
+    return (
+      <HeroDiv img={HandleImage(slugify(HandlePath(this.state.location)))}>
+      <div className="Dark1">
+        <div className="IconsGroup1">
+          <TypesIcons
+            heigth={55}
+            width={30}
+            backColor={"blue"}
+            color={"white"}
+            TextColor={"white"}
+          />
+        </div>
+        <div className="CourseGroup1">
+          <div className="CourseTitle1">
+            {this.props.title}
+          </div>
+          <div className="author1">Par {this.props.author}</div>
+          <div className="date1">{this.props.date}</div>
+        </div>
+      </div>
+    </HeroDiv>
+    );
+  }
 }
 
-var myPath = window.location.pathname;
+export default Hero;
 
-const Hero = props => (
-  <HeroDiv img={HandleImage(slugify(HandlePath(myPath)))}>
-    <div className="Dark1">
-      <div className="IconsGroup1">
-        <TypesIcons
-          heigth={55}
-          width={30}
-          backColor={"blue"}
-          color={"white"}
-          TextColor={"white"}
-        />
-      </div>
-      <div className="CourseGroup1">
-        <div className="CourseTitle1">
-          {props.title}
-        </div>
-        <div className="author1">Par {props.author}</div>
-        <div className="date1">{props.date}</div>
-      </div>
-    </div>
-  </HeroDiv>
-)
-export default Hero
+
 
 const HeroDiv = styled.div`
   grid-column: 2/3;
